54ab2212dbc927516e5dc0b8dc64184e6225dbd2
guimafelipe <guima.felipec@gmail.com>
2025-03-03 22:09:55 -0800

    OAuth process part 1

---
+++ MyAnimeListExtension/OAuthClient.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MyAnimeListExtension;
+internal sealed class OAuthClient : IDisposable
+{
+    private readonly HttpClient _httpClient;
+
+    public OAuthClient()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    private static Uri CreateOAuthRequestUri()
+    {
+        var client_id = Environment.GetEnvironmentVariable("MAL_CLIENT_ID");
+        var code_challenge = Environment.GetEnvironmentVariable("MAL_CODE_CHALLENGE");
+        var redirect_uri = "cmdpalmalext://oauth_redirect_uri/";
+        return new Uri($"https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id={client_id}&code_challenge={code_challenge}&state=state&redirect_uri={redirect_uri}");
+    }
+
+    public static void BeginOAuthRequest()
+    {
+        var uri = CreateOAuthRequestUri();
+        var options = new Windows.System.LauncherOptions();
+        var browserLaunch = false;
+
+        Task.Run(async () =>
+        {
+            browserLaunch = await Windows.System.Launcher.LaunchUriAsync(uri, options);
+
+            if (browserLaunch)
+            {
+                Debug.WriteLine($"Uri Launched - Check browser");
+            }
+            else
+            {
+                Debug.WriteLine($"Uri Launch failed");
+            }
+        });
+    }
+
+    public static void HandleOAuthRedirection(Uri response)
+    {
+        var queryString = response.Query;
+        var queryStringColleection = System.Web.HttpUtility.ParseQueryString(queryString);
+
+        if (queryStringColleection["error"] != null)
+        {
+            // Handle error
+            Debug.WriteLine($"Error: {queryStringColleection["error"]}");
+            return;
+        }
+
+        if (queryStringColleection["code"] == null)
+        {
+            Debug.WriteLine("No code found in the response.");
+            return;
+        }
+
+        // Handle success
+        var code = queryStringColleection["code"];
+        Debug.WriteLine($"Code: {code}");
+
+        // Use the code to get the access token
+
+    }
+
+    public void Dispose()
+    {
+        _httpClient.Dispose();
+    }
+}
--- MyAnimeListExtension/Program.cs
+++ MyAnimeListExtension/Program.cs
@@ -5,43 +5,102 @@
 using System;
 using System.Diagnostics;
 using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.CommandPalette.Extensions;
+using Microsoft.Windows.AppLifecycle;
+using Windows.ApplicationModel.Activation;
 
 namespace MyAnimeListExtension;
 
 public class Program
 {
     [MTAThread]
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
+
+        // Force the app to be single instanced.
+        // Get or register the main instance.
+        var mainInstance = AppInstance.FindOrRegisterForKey("mainInstance");
+        var activationArgs = AppInstance.GetCurrent().GetActivatedEventArgs();
+        if (!mainInstance.IsCurrent)
+        {
+            await mainInstance.RedirectActivationToAsync(activationArgs);
+            return;
+        }
+
+        // Register for activation redirection.
+        AppInstance.GetCurrent().Activated += AppActivationRedirected;
+
         if (args.Length > 0 && args[0] == "-RegisterProcessAsComServer")
         {
-            using ExtensionServer server = new();
-            var extensionDisposedEvent = new ManualResetEvent(false);
-            var extensionInstance = new MyAnimeListExtension(extensionDisposedEvent);
+            try
+            {
+                HandleCOMServerActivation();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error: {ex.Message}");
+            }
 
-
-            var dataProvider = new DataProvider();
-
-            var res = DataProvider.GetAnimeRankingAsync().GetAwaiter().GetResult();
-
-            Console.WriteLine("Eu existo?");
-            Console.WriteLine(res);
-            Debug.WriteLine("Eu existo?");
-            Debug.WriteLine(res);
-
-            // We are instantiating an extension instance once above, and returning it every time the callback in RegisterExtension below is called.
-            // This makes sure that only one instance of SampleExtension is alive, which is returned every time the host asks for the IExtension object.
-            // If you want to instantiate a new instance each time the host asks, create the new instance inside the delegate.
-            server.RegisterExtension(() => extensionInstance);
-
-            // This will make the main thread wait until the event is signalled by the extension class.
-            // Since we have single instance of the extension object, we exit as soon as it is disposed.
-            extensionDisposedEvent.WaitOne();
         }
         else
         {
             Console.WriteLine("Not being launched as a Extension... exiting.");
         }
     }
+
+    private static void AppActivationRedirected(object? sender, Microsoft.Windows.AppLifecycle.AppActivationArguments activationArgs)
+    {
+        // Handle COM server.
+        if (activationArgs.Kind == ExtendedActivationKind.Launch)
+        {
+            var d = activationArgs.Data as ILaunchActivatedEventArgs;
+            var args = d?.Arguments.Split();
+
+            if (args?.Length > 1 && args[1] == "-RegisterProcessAsComServer")
+            {
+                HandleCOMServerActivation();
+            }
+        }
+
+        // Handle Protocol.
+        if (activationArgs.Kind == ExtendedActivationKind.Protocol)
+        {
+            var d = activationArgs.Data as IProtocolActivatedEventArgs;
+            if (d is not null)
+            {
+                HandleProtocolActivation(d.Uri);
+            }
+        }
+    }
+
+    private static void HandleCOMServerActivation()
+    {
+        using ExtensionServer server = new();
+        var extensionDisposedEvent = new ManualResetEvent(false);
+        var extensionInstance = new MyAnimeListExtension(extensionDisposedEvent);
+
+
+        var dataProvider = new DataProvider();
+
+        var res = DataProvider.GetAnimeRankingAsync().GetAwaiter().GetResult();
+
+        Console.WriteLine("Eu existo?");
+        Console.WriteLine(res);
+        Debug.WriteLine("Eu existo?");
+        Debug.WriteLine(res);
+
+        OAuthClient.BeginOAuthRequest();
+
+        // We are instantiating an extension instance once above, and returning it every time the callback in RegisterExtension below is called.
+        // This makes sure that only one instance of SampleExtension is alive, which is returned every time the host asks for the IExtension object.
+        // If you want to instantiate a new instance each time the host asks, create the new instance inside the delegate.
+        server.RegisterExtension(() => extensionInstance);
+
+        // This will make the main thread wait until the event is signalled by the extension class.
+        // Since we have single instance of the extension object, we exit as soon as it is disposed.
+        extensionDisposedEvent.WaitOne();
+    }
+
+    private static void HandleProtocolActivation(Uri oauthRedirectUri) => OAuthClient.HandleOAuthRedirection(oauthRedirectUri);
 }
